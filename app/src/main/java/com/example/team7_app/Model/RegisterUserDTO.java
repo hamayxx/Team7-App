d2a346c41e2b0a7d2a74005b4a9af5dceeb82a0c
dodochi <dodongchien1232@gmail.com>
2022-05-10 22:22:37 +0700

    [Updated] create new user to DB successfully!

---
+++ app/src/main/java/com/example/team7_app/Model/RegisterUserDTO.java
@@ -0,0 +1,13 @@
+package com.example.team7_app.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegisterUserDTO {
+
+    private String login;
+    private String email;
+    private String password;
+}
